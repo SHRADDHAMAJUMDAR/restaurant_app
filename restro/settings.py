a1d6d89e50a4716d673bdf33a3d6ac7bf195980c
SHRADDHAMAJUMDAR <128743516+SHRADDHAMAJUMDAR@users.noreply.github.com>
2025-04-12 02:27:18 +0530

    Update settings.py

--- restro/settings.py
+++ restro/settings.py
@@ -129,11 +129,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-#added by shraddha for email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'shraddhamajumdar03@gmail.com'
-EMAIL_HOST_PASSWORD = 'cmbswefymatussbz'
